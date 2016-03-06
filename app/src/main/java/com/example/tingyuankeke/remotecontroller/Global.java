aaf069ad710e209324c44c1181df7441cd2f0da9
TIngYuanKeke <prelude0390@hotmail.com>
2016-03-06 15:43:15 +0800

    empty the repository

--- app/src/main/java/com/example/tingyuankeke/remotecontroller/Global.java
+++
@@ -1,7 +0,0 @@
-package com.example.tingyuankeke.remotecontroller;
-
-/**
- * Created by TingYuanKeke on 2016/2/23.
- */
-public class Global {
-}
--- app/src/main/java/com/example/tingyuankeke/remotecontroller/MouseControll.java
+++
@@ -1,7 +0,0 @@
-package com.example.tingyuankeke.remotecontroller;
-
-/**
- * Created by TingYuanKeke on 2015/12/3.
- */
-public class MouseControll {
-}
